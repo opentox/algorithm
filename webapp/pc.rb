16dc25252cac951af20b31afe659947b7f6f3dd4
Andreas Maunz <andreas@maunz.de>
2012-09-05 13:23:48 +0200

    Removed code

--- webapp/pc.rb
+++ webapp/pc.rb
@@ -55,53 +55,6 @@ module OpenTox
       raise OpenTox::NotFoundError.new "Unknown descriptor #{params[:descriptor]}."
     end
   end
-  
-  # Run pc descriptor calculation algorithm on dataset for a set of descriptors. Can be constrained to types and libraries.
-  # @param [String] dataset_uri URI of the training dataset
-  # @param optional [String] pc_type Physico-chemical descriptor type to generate, see TODO
-  # @param optional [String] lib Library to use, see TODO
-  # @return [text/uri-list] Task URI
-  post '/pc/AllDescriptors' do
-    response['Content-Type'] = 'text/uri-list'
-    raise OpenTox::NotFoundError.new "Parameter 'dataset_uri' missing." unless params[:dataset_uri]
-  
-    descriptors = YAML::load_file File.join(ENV['HOME'], ".opentox", "config", "pc_descriptors.yaml")
-    params[:pc_type] = descriptors.collect { |id,info| info[:pc_type]}.uniq.sort.join(',')  unless params[:pc_type]
-  
-    task = OpenTox::Task.create("PC descriptor calculation for dataset ", @uri) do |task|
-      Rjb.load(nil,["-Xmx64m"]) # start vm
-      byteArray = Rjb::import('java.io.ByteArrayOutputStream'); printStream = Rjb::import('java.io.PrintStream'); 
-      out = byteArray.new() ; Rjb::import('java.lang.System').out = printStream.new(out) # joelib is too verbose
-      s = Rjb::import('JoelibFc') # import main class
-  
-      LOGGER.debug "Running PC with pc_type '#{params[:pc_type]}' and lib '#{params[:lib]}'"
-      OpenTox::Algorithm.pc_descriptors( { :dataset_uri => params[:dataset_uri], :pc_type => params[:pc_type], :rjb => s, :add_uri => true, :task => task, :lib => params[:lib], :subjectid => @subjectid} )
-    end
-    raise OpenTox::ServiceUnavailableError.newtask.uri+"\n" if task.status == "Cancelled"
-    halt 202,task.uri.to_s+"\n"
-  end
-  
-  # Run pc descriptor calculation algorithm on dataset for a specific descriptor.
-  #
-  # @param [String] dataset_uri URI of the training dataset
-  # @return [text/uri-list] Task URI
-  post '/pc/:descriptor' do
-    response['Content-Type'] = 'text/uri-list'
-    raise OpenTox::NotFoundError.new "Parameter 'dataset_uri' missing." unless params[:dataset_uri]
-  
-    descriptors = YAML::load_file File.join(ENV['HOME'], ".opentox", "config", "pc_descriptors.yaml")
-    params[:pc_type] = descriptors.collect { |id,info| info[:pc_type]}.uniq.sort.join(',')
-  
-    task = OpenTox::Task.create("PC descriptor calculation for dataset ", @uri) do |task|
-      Rjb.load(nil,["-Xmx64m"]) # start vm
-      byteArray = Rjb::import('java.io.ByteArrayOutputStream'); printStream = Rjb::import('java.io.PrintStream'); 
-      out = byteArray.new() ; Rjb::import('java.lang.System').out = printStream.new(out) # joelib is too verbose
-      s = Rjb::import('JoelibFc') # import main class
-      OpenTox::Algorithm.pc_descriptors( { :dataset_uri => params[:dataset_uri], :pc_type => params[:pc_type], :descriptor => params[:descriptor], :rjb => s, :add_uri => false, :task => task, :subjectid => @subjectid} )
-    end
-    raise OpenTox::ServiceUnavailableError.newtask.uri+"\n" if task.status == "Cancelled"
-    halt 202,task.uri.to_s+"\n"
-  end
 
   end
 end
