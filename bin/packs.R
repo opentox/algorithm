731a8dd7528c8159495a70753844db4a4011fba5
ist <ist@istnightly3>
2014-10-16 11:15:46 +0200

    Fixed env variable for R install.

--- bin/packs.R
+++ bin/packs.R
@@ -3,5 +3,5 @@
 
 
 # set mirror to avoid questioning the user
-options(repos=Sys.getenv("R_MIRROR"))
+options(repos=Sys.getenv("CRAN_MIRROR"))
 install.packages(c("caret", "doMC", "e1071", "foreach", "iterators", "kernlab", "multicore", "plyr", "reshape", "randomForest", "RANN", "TunePareto"),lib=Sys.getenv("R_LIBS"))
